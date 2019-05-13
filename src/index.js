9f167d9aeb25911da0dc35202c216d6383b78947
Dominik Michalik <dominik.michalik@vml.com>
2019-05-13 21:50:43 +0200

    Setup router and handle 404 page

--- src/index.js
+++ src/index.js
@@ -1,9 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Route , Link , BrowserRouter as Router} from 'react-router-dom';
+import App from './App';
+import About from './Components/pages/About';
+import Ads from './Components/pages/Ads';
+import Contact from './Components/pages/Contact';
+import Gallery from './Components/pages/Gallery';
+import Intentions from './Components/pages/Intentions';
+import Notfound from './Components/pages/Notfound';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routing = (
+    <Router>
+        <div>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/about-us">About</Link>
+                </li>
+                <li>
+                    <Link to="/ads">Ads</Link>
+                </li>
+                <li>
+                    <Link to="/contact">Contact</Link>
+                </li>
+                <li>
+                    <Link to="/gallery">Gallery</Link>
+                </li>
+                <li>
+                    <Link to="/intentions">Intentions</Link>
+                </li>
+            </ul>
+            <Route exact path="/" component={App}  />
+            <Route path="/about-us" component={About} />
+            <Route path="/ads" component={Ads} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/gallery" component={Gallery} />
+            <Route path="/intentions" component={Intentions} />
+            <Route component={Notfound} />
+        </div>
+    </Router>
+);
+
+
+ReactDOM.render(routing, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
