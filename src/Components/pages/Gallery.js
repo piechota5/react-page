46d91843a28c9fc3766e8c55fef78b21de31c259
Dominik Michalik <dominik.michalik@vml.com>
2019-05-22 19:40:15 +0200

    Create gallery popup

--- src/Components/pages/Gallery.js
+++ src/Components/pages/Gallery.js
@@ -3,27 +3,49 @@ import Copyright from "../sections/homepage/Copyright";
 
 class Gallery extends Component{
 
+    constructor(props){
+        super(props);
+
+        this.state = {
+            popupImage: ''
+        }
+    }
+
     componentDidMount() {
         this.getGalleryImages();
     }
 
+
     getGalleryImages = () => {
+
+        let images = [];
 
         let req = require.context("../../img/gallery", false, /.*\.jpg$/);
         req.keys().forEach(function(key){
             req(key);
-            console.log(req(key));
-            document.querySelector('.gallery__inner').innerHTML +=
-                `
-                <figure class="gallery__image">
-                    <img src=${req(key)} class="gallery__img" alt />
-                </figure>
-                `
-            ;
+            images.push(req(key));
         });
+
+        return images;
+
     };
 
+    handlePopupImage = (image) => {
+        this.setState({
+            popupImage: image
+        });
+
+        document.querySelector('.gallery-modal').classList.add('isOpen');
+    };
+
+    hideGalleryPopup(){
+        document.querySelector('.gallery-modal').classList.remove('isOpen');
+    }
+
     render() {
+
+        const galleryImages = this.getGalleryImages();
+
 
         return(
             <>
@@ -34,10 +56,21 @@ class Gallery extends Component{
                 <section className="gallery">
                     <div className="container">
                         <div className="gallery__inner">
-
+                            {galleryImages.map((image,index) => (
+                                <figure className="gallery__image" key={index}>
+                                    <img
+                                        src={image}
+                                        alt="gallery"
+                                        className="gallery__img"
+                                        onClick={() => this.handlePopupImage(image)}/>
+                                </figure>
+                            ))}
                         </div>
                     </div>
                 </section>
+                <div className="gallery-modal" onClick={() => this.hideGalleryPopup()}>
+                    <img src={this.state.popupImage} alt="gallery" />
+                </div>
                 <Copyright/>
             </>
         )
